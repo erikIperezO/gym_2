380486ea0b7891570f3676892a18dba750933005
Erik <ing.erikperez.o@gmail.com>
2023-03-10 17:34:08 -0600

    login

--- client/gym/src/App.tsx
+++ client/gym/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Home from 'pages/Home';
-import Navbar from 'components/Navbar';
+// import Home from 'pages/Home';
+// import Navbar from 'components/Navbar';
+// import Sidebar from 'components/Sidebar';
+import Login from 'pages/Login';
 
 const App: React.FC = () => {
   return (
     <>
-      <Navbar />
-      <Home />
+      <Login />
     </>
   );
 };
